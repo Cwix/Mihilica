018c4624e2dbf226539367240b9e44b76e5bf2a4
Danijel Cw1X Predojevic <danijel@vivant.eco>
2021-08-29 12:08:31 +0200

    Add two output channels Rename some variables

--- src/main.cpp
+++ src/main.cpp
@@ -17,34 +17,72 @@ int ch1MaxInputSignal = 2100;
 int ch2MinInputSignal = 900;
 int ch2MaxInputSignal = 2100;
  
-int M1RPWM_Output = 5; // Arduino PWM output pin 5; connect to IBT-2 pin 1 (RPWM)
-int M1LPWM_Output = 6; // Arduino PWM output pin 6; connect to IBT-2 pin 2 (LPWM)
+//PIN Configurations
+const int M1RPWM_OUTPUT = 5; // Arduino PWM output pin 5; connect to IBT-2 pin 1 (RPWM)
+const int M1LPWM_OUTPUT = 6; // Arduino PWM output pin 6; connect to IBT-2 pin 2 (LPWM)
 
-int M2RPWM_Output = 10;
-int M2LPWM_Output = 11;
+const int M2RPWM_OUTPUT = 10;
+const int M2LPWM_OUTPUT = 11;
+
+const int CH1 = 2;
+const int CH2 = 2;
+const int CH3 = 23;
+const int CH4 = 24;
+
+const int ENGINE_ENABLE_OUT = 7;
+const int STARTER_OUTPUT = 8;
+
+boolean throttleEnabled = 0;
 
 void setup() 
 {
   Serial.begin(115200);
 
-  pinMode(M1RPWM_Output, OUTPUT);
-  pinMode(M1LPWM_Output, OUTPUT);
+  pinMode(M1RPWM_OUTPUT, OUTPUT);
+  pinMode(M1LPWM_OUTPUT, OUTPUT);
 
-  pinMode(M2RPWM_Output, OUTPUT);
-  pinMode(M2LPWM_Output, OUTPUT);
+  pinMode(M2RPWM_OUTPUT, OUTPUT);
+  pinMode(M2LPWM_OUTPUT, OUTPUT);
+
+  pinMode(CH3, INPUT);
+  pinMode(CH4, INPUT);
+
+  pinMode(ENGINE_ENABLE_OUT, OUTPUT);
+  pinMode(STARTER_OUTPUT, OUTPUT);
 
   attachInterrupt(digitalPinToInterrupt(2), risingCh1, RISING);
   attachInterrupt(digitalPinToInterrupt(3), risingCh2, RISING);
+
+  Serial.println("Mihilica ready.");
 
   // calibrate();
 }
  
 void loop() 
 {
-  // Serial.print("Channel 1 val: ");
-  // Serial.println(ch1_value);
+  Serial.print("CH3 val: ");
+  Serial.println(analogRead(CH3));
+
+  Serial.print("CH4 val: ");
+  Serial.println(analogRead(CH4));
 
   handleIdle();
+
+  if(analogRead(CH3) > 700) {
+      throttleEnabled = 1;
+      digitalWrite(ENGINE_ENABLE_OUT, HIGH);
+  } else {
+      throttleEnabled = 0;
+      digitalWrite(ENGINE_ENABLE_OUT, LOW);
+  }
+
+
+  if(analogRead(CH4) > 700) {
+    digitalWrite(STARTER_OUTPUT, HIGH);
+  } else {
+    digitalWrite(STARTER_OUTPUT, LOW);
+  }
+
 
   /** Motor 1 **/
   if(!isIdle(ch1_value)) {
@@ -52,34 +90,34 @@ void loop()
     {
       int motor1Value = map(ch1_value, 1500, ch1MaxInputSignal, 0, 255);
       // reverse rotation
-      analogWrite(M1LPWM_Output, 0);
-      analogWrite(M1RPWM_Output, motor1Value);
+      analogWrite(M1LPWM_OUTPUT, 0);
+      analogWrite(M1RPWM_OUTPUT, motor1Value);
     }
     else
     {
       int motor1Value = map(ch1_value, ch1MinInputSignal, 1500, 255, 0);
       // forward rotation
-      analogWrite(M1RPWM_Output, 0);
-      analogWrite(M1LPWM_Output, motor1Value);
+      analogWrite(M1RPWM_OUTPUT, 0);
+      analogWrite(M1LPWM_OUTPUT, motor1Value);
     }
   }
   
 
   /** Motor 2 **/
-  if(!isIdle(ch2_value)) {
+  if(!isIdle(ch2_value) && throttleEnabled) {
     if (ch2_value > 1500)
     {
       int motor2Value = map(ch2_value, 1500, ch2MaxInputSignal, 0, 255);
       // reverse rotation
-      analogWrite(M2LPWM_Output, 0);
-      analogWrite(M2RPWM_Output, motor2Value);
+      analogWrite(M2LPWM_OUTPUT, 0);
+      analogWrite(M2RPWM_OUTPUT, motor2Value);
     }
     else
     {
       int motor2Value = map(ch2_value, ch2MinInputSignal, 1500, 255, 0);
       // forward rotation
-      analogWrite(M2RPWM_Output, 0);
-      analogWrite(M2LPWM_Output, motor2Value);
+      analogWrite(M2RPWM_OUTPUT, 0);
+      analogWrite(M2LPWM_OUTPUT, motor2Value);
     }
   }
   
@@ -141,14 +179,14 @@ bool isIdle(int channelValue)
 void handleIdle() {
   if(isIdle(ch1_value)) 
   {
-    analogWrite(M1RPWM_Output, 0);
-    analogWrite(M1LPWM_Output, 0);
+    analogWrite(M1RPWM_OUTPUT, 0);
+    analogWrite(M1LPWM_OUTPUT, 0);
   }
 
   if(isIdle(ch2_value)) 
   {
-    analogWrite(M2RPWM_Output, 0);
-    analogWrite(M2LPWM_Output, 0);
+    analogWrite(M2RPWM_OUTPUT, 0);
+    analogWrite(M2LPWM_OUTPUT, 0);
   }
 }
 
