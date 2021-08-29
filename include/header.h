a425ebba55ee75080520cd9c9edb18888c7c71c8
Danijel Cw1X Predojevic <danijel@vivant.eco>
2021-08-29 11:43:25 +0200

    Calibration WIP

--- include/header.h
+++ include/header.h
@@ -4,5 +4,7 @@ void fallingCh1();
 void risingCh2();
 void fallingCh2();
 
+void calibrate();
+
 bool isIdle(int);
 void handleIdle();
--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,11 @@ const int deadband = 50;
 const int centerSignal = 1500;
 
 /** @todo figure out calibration **/
-int minInputSignal = 900;
-int maxInputSignal = 2100;
+int ch1MinInputSignal = 900;
+int ch1MaxInputSignal = 2100;
+
+int ch2MinInputSignal = 900;
+int ch2MaxInputSignal = 2100;
  
 int M1RPWM_Output = 5; // Arduino PWM output pin 5; connect to IBT-2 pin 1 (RPWM)
 int M1LPWM_Output = 6; // Arduino PWM output pin 6; connect to IBT-2 pin 2 (LPWM)
@@ -22,37 +25,39 @@ int M2LPWM_Output = 11;
 
 void setup() 
 {
+  Serial.begin(115200);
+
   pinMode(M1RPWM_Output, OUTPUT);
   pinMode(M1LPWM_Output, OUTPUT);
 
   pinMode(M2RPWM_Output, OUTPUT);
   pinMode(M2LPWM_Output, OUTPUT);
 
-  pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);
-
-  // Serial.begin(115200);
-
   attachInterrupt(digitalPinToInterrupt(2), risingCh1, RISING);
   attachInterrupt(digitalPinToInterrupt(3), risingCh2, RISING);
+
+  // calibrate();
 }
  
 void loop() 
 {
+  // Serial.print("Channel 1 val: ");
+  // Serial.println(ch1_value);
+
   handleIdle();
 
   /** Motor 1 **/
   if(!isIdle(ch1_value)) {
     if (ch1_value > 1500)
     {
-      int motor1Value = map(ch1_value, 1500, maxInputSignal, 0, 255);
+      int motor1Value = map(ch1_value, 1500, ch1MaxInputSignal, 0, 255);
       // reverse rotation
       analogWrite(M1LPWM_Output, 0);
       analogWrite(M1RPWM_Output, motor1Value);
     }
     else
     {
-      int motor1Value = map(ch1_value, minInputSignal, 1500, 255, 0);
+      int motor1Value = map(ch1_value, ch1MinInputSignal, 1500, 255, 0);
       // forward rotation
       analogWrite(M1RPWM_Output, 0);
       analogWrite(M1LPWM_Output, motor1Value);
@@ -64,20 +69,65 @@ void loop()
   if(!isIdle(ch2_value)) {
     if (ch2_value > 1500)
     {
-      int motor2Value = map(ch2_value, 1500, maxInputSignal, 0, 255);
+      int motor2Value = map(ch2_value, 1500, ch2MaxInputSignal, 0, 255);
       // reverse rotation
       analogWrite(M2LPWM_Output, 0);
       analogWrite(M2RPWM_Output, motor2Value);
     }
     else
     {
-      int motor2Value = map(ch2_value, minInputSignal, 1500, 255, 0);
+      int motor2Value = map(ch2_value, ch2MinInputSignal, 1500, 255, 0);
       // forward rotation
       analogWrite(M2RPWM_Output, 0);
       analogWrite(M2LPWM_Output, motor2Value);
     }
   }
   
+}
+
+void calibrate() {
+  // turn on LED to signal the start of the calibration period:
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+
+  // calibrate during the first five seconds
+  while (millis() < 5000) {
+    Serial.println(ch1_value);
+    // record the maximum sensor value
+    if(ch1_value > 800 && ch1_value < 2500) { //Sanity check
+      if (ch1_value > ch1MaxInputSignal) {
+      ch1MaxInputSignal = ch1_value;
+      }
+
+      // record the minimum sensor value
+      if (ch1_value < ch1MinInputSignal) {
+        ch1MinInputSignal = ch1_value;
+      }
+    }
+
+    // record the maximum sensor value
+    if (ch2_value > ch2MaxInputSignal) {
+      ch2MaxInputSignal = ch2_value;
+    }
+
+    // record the minimum sensor value
+    if (ch2_value < ch2MinInputSignal) {
+      ch2MinInputSignal = ch2_value;
+    }
+  }
+
+  Serial.print("Channel 1 min: ");
+  Serial.println(ch1MinInputSignal);
+  Serial.print("Channel 1 max: ");
+  Serial.println(ch1MaxInputSignal);
+
+  Serial.print("Channel 2 min: ");
+  Serial.println(ch2MinInputSignal);
+  Serial.print("Channel 2 max: ");
+  Serial.println(ch2MaxInputSignal);
+
+  // signal the end of the calibration period
+  digitalWrite(13, LOW);
 }
 
 bool isIdle(int channelValue) 
