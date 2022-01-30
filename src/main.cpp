53a5fb268fcd858706be83d18e34d9ee64fc96dd
Danijel Cw1X Predojevic <danijel@vivant.eco>
2022-01-30 19:19:40 +0100

    Some blind refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,25 @@
 #include <Arduino.h>
 #include "../include/header.h"
 
-volatile int ch1_value = 0;
+//CH1 and CH2 are RC values (1000-2000) so we set them at 1500 to zero them out
+volatile int ch1_value = 1500;
 volatile int ch1_prev_time = 0;
 
-volatile int ch2_value = 0;
+volatile int ch2_value = 1500;
 volatile int ch2_prev_time = 0;
+
+//These are analogRead values (0-1024) so we set them at 0 to zero them out
+volatile int ch3_value = 0;
+volatile int ch4_value = 0;
 
 const int deadband = 50;
 const int centerSignal = 1500;
 
-/** @todo figure out calibration **/
+/**
+ * Some sane values for startup
+ * 
+ * @todo figure out calibration 
+ * **/
 int ch1MinInputSignal = 900;
 int ch1MaxInputSignal = 2100;
 
@@ -25,7 +34,7 @@ const int M2RPWM_OUTPUT = 10;
 const int M2LPWM_OUTPUT = 11;
 
 const int CH1 = 2;
-const int CH2 = 2;
+const int CH2 = 3;
 const int CH3 = 23;
 const int CH4 = 24;
 
@@ -50,6 +59,9 @@ void setup()
   pinMode(ENGINE_ENABLE_OUT, OUTPUT);
   pinMode(STARTER_OUTPUT, OUTPUT);
 
+  digitalWrite(ENGINE_ENABLE_OUT, engineEnabled);
+  digitalWrite(STARTER_OUTPUT, LOW);
+
   attachInterrupt(digitalPinToInterrupt(2), risingCh1, RISING);
   attachInterrupt(digitalPinToInterrupt(3), risingCh2, RISING);
 
@@ -60,6 +72,12 @@ void setup()
  
 void loop() 
 {
+  Serial.print("CH1 val: ");
+  Serial.println(ch1_value);
+
+  Serial.print("CH2 val: ");
+  Serial.println(ch2_value);
+
   Serial.print("CH3 val: ");
   Serial.println(analogRead(CH3));
 
@@ -84,27 +102,27 @@ void loop()
   }
 
 
-  /** Motor 1 **/
-  if(!isIdle(ch1_value)) {
-    if (ch1_value > 1500)
-    {
-      int motor1Value = map(ch1_value, 1500, ch1MaxInputSignal, 0, 255);
-      // reverse rotation
-      analogWrite(M1LPWM_OUTPUT, 0);
-      analogWrite(M1RPWM_OUTPUT, motor1Value);
+  if(engineEnabled) {
+    /** Motor 1 **/
+    if(!isIdle(ch1_value)) {
+      if (ch1_value > 1500)
+      {
+        int motor1Value = map(ch1_value, 1500, ch1MaxInputSignal, 0, 255);
+        // reverse rotation
+        analogWrite(M1LPWM_OUTPUT, 0);
+        analogWrite(M1RPWM_OUTPUT, motor1Value);
+      }
+      else
+      {
+        int motor1Value = map(ch1_value, ch1MinInputSignal, 1500, 255, 0);
+        // forward rotation
+        analogWrite(M1RPWM_OUTPUT, 0);
+        analogWrite(M1LPWM_OUTPUT, motor1Value);
+      }
     }
-    else
-    {
-      int motor1Value = map(ch1_value, ch1MinInputSignal, 1500, 255, 0);
-      // forward rotation
-      analogWrite(M1RPWM_OUTPUT, 0);
-      analogWrite(M1LPWM_OUTPUT, motor1Value);
-    }
-  }
-  
 
-  /** Motor 2 **/
-  if(!isIdle(ch2_value) && engineEnabled) {
+    /** Motor 2 **/
+  if(!isIdle(ch2_value)) {
     if (ch2_value > 1500)
     {
       int motor2Value = map(ch2_value, 1500, ch2MaxInputSignal, 0, 255);
@@ -120,7 +138,7 @@ void loop()
       analogWrite(M2LPWM_OUTPUT, motor2Value);
     }
   }
-  
+  }
 }
 
 void calibrate() {
@@ -170,7 +188,7 @@ void calibrate() {
 
 bool isIdle(int channelValue) 
 {
-  return channelValue > centerSignal - deadband && channelValue < centerSignal + deadband;
+  return channelValue > (centerSignal - deadband) && channelValue < (centerSignal + deadband);
 }
 
 /** 
@@ -196,21 +214,21 @@ void handleIdle() {
  * 
  *****/
 void risingCh1() {
-  attachInterrupt(digitalPinToInterrupt(2), fallingCh1, FALLING);
+  attachInterrupt(digitalPinToInterrupt(CH1), fallingCh1, FALLING);
   ch1_prev_time = micros();
 }
 
 void fallingCh1() {
-  attachInterrupt(digitalPinToInterrupt(2), risingCh1, RISING);
+  attachInterrupt(digitalPinToInterrupt(CH1), risingCh1, RISING);
   ch1_value = micros() - ch1_prev_time;
 }
 
 void risingCh2() {
-  attachInterrupt(digitalPinToInterrupt(3), fallingCh2, FALLING);
+  attachInterrupt(digitalPinToInterrupt(CH2), fallingCh2, FALLING);
   ch1_prev_time = micros();
 }
  
 void fallingCh2() {
-  attachInterrupt(digitalPinToInterrupt(3), risingCh2, RISING);
+  attachInterrupt(digitalPinToInterrupt(CH2), risingCh2, RISING);
   ch2_value = micros() - ch2_prev_time;
 }
